039e3f4ddf203bfc94b0f734014e5ffceeba0f07
Simple and Tiny Utilities <108601122+yudai-tiny-developer@users.noreply.github.com>
2025-08-12 16:48:12 +0900

    v1.1.0 (#1)
    Co-authored-by: Simple and Tiny Utilities <yudai-tiny-developer@gmail.com>

--- inject.js
+++ inject.js
@@ -1,19 +1,43 @@
 var _post_screenshot_canvas;
 
 (() => {
+    function getVideo() {
+        const video = document.body.querySelector('video');
+        if (video) {
+            return video;
+        }
+
+        for (const iframe of document.body.querySelectorAll('iframe')) {
+            try {
+                const doc = iframe.contentDocument || iframe.contentWindow.document;
+                if (doc) {
+                    const found = getVideo(doc);
+                    if (found) {
+                        return found;
+                    }
+                }
+            } catch (e) {
+                // CORS Error
+            }
+        }
+
+        return null;
+    }
+
     try {
-        const video = document.querySelector('video') ?? document.querySelector('iframe')?.contentWindow.document.querySelector('video');
+        const video = getVideo();
+        if (video) {
+            _post_screenshot_canvas = _post_screenshot_canvas ?? document.createElement('canvas');
+            _post_screenshot_canvas.width = video.videoWidth;
+            _post_screenshot_canvas.height = video.videoHeight;
 
-        _post_screenshot_canvas = _post_screenshot_canvas ?? document.createElement('canvas');
-        _post_screenshot_canvas.width = video.videoWidth;
-        _post_screenshot_canvas.height = video.videoHeight;
+            const context = _post_screenshot_canvas.getContext('2d');
+            context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
 
-        const context = _post_screenshot_canvas.getContext('2d');
-        context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+            const hashtags = [...document.title.matchAll(/[#＃]([\p{L}\p{N}_-]+)/gu)].map(m => m[1]).filter(tag => !/^\p{N}+$/u.test(tag)).join(',');
 
-        const hashtags = (document.title.match(/#[\p{L}\p{N}_]+/gu) || []).map(tag => tag.slice(1)).join(',');
-
-        chrome.runtime.sendMessage({ msg: 'ScreenShot', base64png: _post_screenshot_canvas.toDataURL().replace(/^data:image\/png;base64,/, ''), hashtags });
+            chrome.runtime.sendMessage({ msg: 'ScreenShot', base64image: _post_screenshot_canvas.toDataURL('image/jpeg', 0.85).replace(/^data:image\/\w+;base64,/, ''), hashtags });
+        }
     } catch {
         // service_worker not ready
     }
