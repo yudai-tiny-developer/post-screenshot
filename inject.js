298a6916bba5b624bc73081a4936f899dbf88fc2
Simple and Tiny Utilities <yudai-tiny-developer@gmail.com>
2025-08-16 05:34:19 +0900

    ui update

--- inject.js
+++ inject.js
@@ -25,41 +25,81 @@ var _post_screenshot_dialog;
                 }
 
                 if (common.value(data.seek, common.default_seek)) {
+                    let pushInterval;
+
                     function close() {
+                        clearInterval(pushInterval);
                         _post_screenshot_dialog.style.display = 'none';
                         video.play();
+                    }
+
+                    function step_back() {
+                        video.currentTime -= 1.0 / 60.0;
+                    }
+
+                    function step_forward() {
+                        video.currentTime += 1.0 / 60.0;
+                    }
+
+                    function rewind() {
+                        video.currentTime -= 1.0;
+                    }
+
+                    function fast_forward() {
+                        video.currentTime += 1.0;
+                    }
+
+                    function take_screenshot() {
+                        close();
+                        screenshot();
                     }
 
                     function seek(e) {
                         if (e.key === 'ArrowLeft') {
                             e.preventDefault();
-                            video.currentTime -= 1.0 / 60.0;
+                            step_back();
                         } else if (e.key === 'ArrowRight') {
                             e.preventDefault();
-                            video.currentTime += 1.0 / 60.0;
+                            step_forward();
+                        } else if (e.key === 'ArrowUp') {
+                            e.preventDefault();
+                            rewind();
+                        } else if (e.key === 'ArrowDown') {
+                            e.preventDefault();
+                            fast_forward();
                         } else if (e.key === 'Enter') {
                             e.preventDefault();
+                            take_screenshot();
+                        } else if (e.key === 'Escape') {
+                            e.preventDefault();
                             close();
-                            screenshot();
                         }
                     }
 
                     if (!_post_screenshot_dialog) {
                         _post_screenshot_dialog = document.createElement('dialog');
                         _post_screenshot_dialog.id = '_post_screenshot_dialog';
-                        _post_screenshot_dialog.addEventListener('blur', close);
+                        _post_screenshot_dialog.addEventListener('focusout', (e) => {
+                            if (!_post_screenshot_dialog.contains(e.relatedTarget)) {
+                                close();
+                            }
+                        });
                         _post_screenshot_dialog.addEventListener('keydown', seek);
                         _post_screenshot_dialog.style.position = 'fixed';
                         _post_screenshot_dialog.style.backgroundColor = 'black';
                         _post_screenshot_dialog.style.color = 'white';
-                        _post_screenshot_dialog.style.fontSize = '16px';
+                        _post_screenshot_dialog.style.fontSize = '14px';
                         _post_screenshot_dialog.style.margin = 0;
                         _post_screenshot_dialog.style.zIndex = video.style.zIndex + 1;
+                        _post_screenshot_dialog.style.outline = 'none';
 
                         const keyActions = [
-                            { key: "←", action: "Step back" },
-                            { key: "→", action: "Step forward" },
-                            { key: "Enter", action: "Take a screenshot" }
+                            { key: '←', action: 'Step back', callback: step_back },
+                            { key: '→', action: 'Step forward', callback: step_forward },
+                            { key: '↑', action: 'Rewind 1 second', callback: rewind },
+                            { key: '↓', action: 'Fast forward 1 second', callback: fast_forward },
+                            { key: 'Enter', action: 'Take a screenshot', callback: take_screenshot },
+                            { key: 'Escape', action: 'Cancel', callback: close },
                         ];
 
                         const tableDiv = document.createElement('div');
@@ -72,7 +112,13 @@ var _post_screenshot_dialog;
                             const cellKey = document.createElement('div');
                             cellKey.style.display = 'table-cell';
                             cellKey.style.textAlign = 'center';
-                            cellKey.textContent = item.key;
+                            const cellKeyButton = document.createElement('input');
+                            cellKeyButton.type = 'button';
+                            cellKeyButton.value = item.key;
+                            cellKeyButton.addEventListener('click', item.callback);
+                            cellKeyButton.addEventListener('mousedown', () => { clearInterval(pushInterval); pushInterval = setInterval(item.callback, 100); });
+                            cellKeyButton.addEventListener('mouseup', () => { clearInterval(pushInterval); });
+                            cellKey.appendChild(cellKeyButton);
 
                             const cellSeparator = document.createElement('div');
                             cellSeparator.style.display = 'table-cell';
